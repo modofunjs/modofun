69b3eca935558c67dd7b0ed645fe2cd71a48a628
Filipe Tavares <fptavares@gmail.com>
2018-08-25 20:54:06 +0200

    Added test for empty request, and for path parsing on vanilla http

--- test/others.js
+++ test/others.js
@@ -9,11 +9,31 @@ describe('Vanilla HTTP IncomingMessage', function() {
       request.url = '/test/with/path?and=querystring';
       modofun.gcloud({ test: (req) => {
         expect(req.params).to.have.lengthOf(2);
+        expect(req.params).to.deep.equal(['with', 'path']);
         done();
-      }}, { mode: 'reqres' })(request, null, err => err && done(err));
+      }}, {
+        mode: 'reqres',
+        errorHandler: err => err && done(err)
+      })(request, null);
     }
   }
   it('should support not having req.path, only req.url', testNoReqPath());
+});
+
+describe('Empty request', function() {
+  function testEmptyRequest() {
+    return function(done) {
+      modofun.gcloud({ test: () => {
+        done(new Error('Should have failed!'));
+      }}, {
+        errorHandler: err => {
+          expect(err).to.be.an('error');
+          done();
+        }
+      })({}, {});
+    }
+  }
+  it('should fail and throw an error', testEmptyRequest());
 });
 
 describe('General bad config', function() {
@@ -21,7 +41,7 @@ describe('General bad config', function() {
     return function(done) {
       try {
         modofun({ test: () => {} }, { type: 'hahaha' })();
-        done('Should have thrown exception');
+        done(new Error('Should have thrown exception'));
       } catch(err) {
         expect(err).to.be.an('error');
         done();
